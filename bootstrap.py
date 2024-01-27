1dff6515708641be166d958eed65eec3ccedb171
Emia <raojin@max-optics.com>
2024-01-27 14:29:24 +0800

    style: format

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,14 @@
+from concurrent.futures import ProcessPoolExecutor
+
 import app
 import view
-
-from concurrent.futures import ProcessPoolExecutor
 
 
 def main():
     with ProcessPoolExecutor(max_workers=2) as executor:
         executor.submit(app.main)
         executor.submit(view.main)
-        
-        
-        
+
+
 if __name__ == '__main__':
     main()
--- view.py
+++ view.py
@@ -1,22 +1,16 @@
-import gradio
 import webview
-import threading
-import time
-import queue
+
+server_name = '127.0.0.1'
+server_port = 7860
 
 
-server_name="127.0.0.1"
-server_port=7860
-
-    
-    
 def main():
-    url = f"http://{server_name}:{server_port}"
+    url = f'http://{server_name}:{server_port}'
     # url = "https://pywebview.flowrl.com/guide/"
     webview.create_window('Hello world', url)
     webview.start()
     raise SystemExit(0)
-    
-    
+
+
 if __name__ == '__main__':
     main()
